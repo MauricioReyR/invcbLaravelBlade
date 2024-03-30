8b38211184b7a73bd37ecf0513551590a5795343
Mauricio Rey <mauricioreygmail.com>
2024-03-29 19:02:21 -0500

    roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       $role1 = Role::create(['name' => 'Gerente']);
+       $role2 = Role::create(['name' => 'Admin']);
+       $role3 = Role::create(['name' => 'Vendedores']);
+       $role4 = Role::create(['name' => 'Tecnico']);
+       $role5 = Role::create(['name' => 'Clientes']);
+       $role6 = Role::create(['name' => 'Contabilidad']);
+
+       
+       Permission::create(['name']);
+    }
+}
